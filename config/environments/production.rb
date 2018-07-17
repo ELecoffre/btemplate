19612cc6448e649a565dfabb0c9a1ae9f7e108fe
ELecoffre <elecoffre.contact@gmail.com>
2018-07-17 20:16:38 +0200

    cancel previous

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -16,7 +16,7 @@ Rails.application.configure do
 
   #HERE
   # set the relative root, because we're deploying to /myrailsapp
-  config.action_controller.relative_url_root  = "/myrailsapp"
+
 
   # Attempt to read encrypted secrets from `config/secrets.yml.enc`.
   # Requires an encryption key in `ENV["RAILS_MASTER_KEY"]` or
